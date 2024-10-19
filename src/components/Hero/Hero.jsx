82bebeb0850a404bf386ba5499a259b46e7b106f
kramislife <egariando619@gmail.com>
2024-10-19 15:43:26 +0800

    Add resume download button to Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
 import profile from "@assets/profile.png";
+import resume from "@assets/resume.pdf";
+import { Button } from "@/components/ui/button";
 
 const Hero = () => {
   return (
     <section>
-      <div className="container px-4 lg:mx-auto flex flex-col lg:flex-row items-center lg:space-x-10">
+      <div className="container px-4 lg:mx-auto flex flex-col lg:flex-row items-center lg:space-x-10 lg:mt-0 mt-10">
         {/* Text Content */}
         <div className="lg:w-1/2 text-neutral-300 lg:space-y-8">
           <h1 className="text-4xl lg:text-7xl font-bold mb-2">
@@ -14,12 +15,19 @@ const Hero = () => {
             Front-end Web Developer | React Developer
           </span>
           <p className="text-md lg:text-lg lg:leading-relaxed mt-4">
-            I'm a passionate Front-End Developer skilled in building responsive, dynamic web
-            applications with ReactJS, HTML, and CSS (Tailwind). Experienced in
-            creating clean code, user-friendly interfaces, and optimizing
-            performance. Strong in state management and component-based
-            architecture, with a focus on delivering seamless user experiences.
+            I'm a passionate Front-End Developer skilled in building responsive,
+            dynamic web applications with ReactJS, HTML, and CSS (Tailwind).
+            Experienced in creating clean code, user-friendly interfaces, and
+            optimizing performance. Strong in state management and
+            component-based architecture, with a focus on delivering seamless
+            user experiences.
           </p>
+          <Button variant="secondary" className="lg:mt-0 mt-5 lg:w-auto w-full">
+            <a href={resume} target="_blank" className="text-lg">
+              {" "}
+              Download Resume
+            </a>{" "}
+          </Button>
         </div>
 
         {/* Image */}
