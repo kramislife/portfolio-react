92566e9fb173cddb0af7569064175b50c6a207e2
egariando <gariandoerick62@gmail.com>
2024-10-30 20:55:28 +0800

    Add comment to Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavItems } from "../constant/Index";
 import logo from "@assets/logo.png";
 
+// ssdada
 const Navbar = () => {
   return (
     <nav className="w-full">
